02f2e370cace3b2ae4de64c8f6c04ae9d3180545
Chen Chen <1948015867@qq.com>
2025-07-11 19:06:52 +0900

    [Servo] modification version for enable pwm servo control

--- aerial_robot_nerve/spinal/mcu_project/lib/Jsk_Lib/servo/drivers/GX3240MGV1/gx_pwm_servo_driver.h
+++ aerial_robot_nerve/spinal/mcu_project/lib/Jsk_Lib/servo/drivers/GX3240MGV1/gx_pwm_servo_driver.h
@@ -3,63 +3,55 @@
 
 #include <array>
 #include <cmath>
+#include <cstdint>
 
 #include "config.h"
+#include "flashmemory/flashmemory.h"
+#include "servo/drivers/servo_base_driver.h"
+#include "stm32h7xx_hal.h"  // Add this include for HAL types
 
-class GxPwmServo {
+constexpr float MIN_PULSE_WIDTH_US = 500.0f;
+constexpr float MAX_PULSE_WIDTH_US = 2500.0f;
+constexpr float ANGLE_RANGE_RAD = 357.0f * M_PI / 180.0f;
+
+class GxPwmServo : public ServoBase {
    public:
-    // Constructor now takes a reference, guaranteeing it's not null.
-    GxPwmServo(uint8_t id, TIM_HandleTypeDef& htim, uint32_t tim_channel);
+    GxPwmServo() = default;
     ~GxPwmServo() = default;
 
-    GxPwmServo(const GxPwmServo&) = delete;
-    GxPwmServo& operator=(const GxPwmServo&) = delete;
-    GxPwmServo(GxPwmServo&&) = delete;
-    GxPwmServo& operator=(GxPwmServo&&) = delete;
+    bool init(TIM_HandleTypeDef* htim, uint8_t servo_num);
+    void init(UART_HandleTypeDef* huart, osMutexId* mutex = NULL) override {}
+    void update() override;
+    void setTorque(uint8_t servo_index) override;
+    void setTorqueFromPresetnPos(uint8_t servo_index) override;
+    void setAndSaveInitPosition(uint8_t id, float angle_rad);
+    float angleToPulseWidth(float angle_rad) const;
+    void setCCR(uint32_t channel, float pulse_us);
 
-    void init();
-    void setGoalAngle(float angle_rad);
-    void torqueEnable(bool enable);
-
-    inline uint8_t getId() const { return id_; }
-    inline bool isTorqueEnabled() const { return torque_enabled_; }
-    inline float getGoalAngleRad() const { return goal_angle_rad_; }
-
-   private:
-    uint8_t id_;
-    TIM_HandleTypeDef& htim_;
-    uint32_t tim_channel_;
-    uint32_t max_pwm_count_;
-    bool torque_enabled_;
-    float last_pulse_us_;
-    float goal_angle_rad_;
-
-    float angleToPulseWidth(float angle_rad);
-    void setCCR(float pulse_us);
-};
-
-class GxPwmServoHandler {
-   public:
-    GxPwmServoHandler() = default;
-    ~GxPwmServoHandler() = default;
-
-    bool init(uint8_t servo_num);
-
-    void setGoalAngle(uint8_t id, float angle_rad);
-    void torqueEnable(uint8_t id, bool enable);
-
-    void setSyncGoalAngle(const uint8_t* ids, const float* angles_rad, size_t size);
-
-    inline void update() {}  // Does nothing for PWM servos without feedback
-    inline size_t getServoNum() const { return servo_count_; }
-    const std::array<GxPwmServo, 4>& getServos() const { return servos_; }
-    inline void setGoalPosition(uint8_t id, float angle_rad) { setGoalAngle(id, angle_rad); }
-    inline void setTorque(uint8_t id, bool enable) { torqueEnable(id, enable); }
+    // Implement all pure virtual functions from ServoBase
+    void pinReconfig() override {}
+    void ping() override {}
+    HAL_StatusTypeDef read(uint8_t* data, uint32_t timeout) override {
+        return HAL_OK;
+    }
+    void reboot(uint8_t servo_index) override {}
+    void setHomingOffset(uint8_t servo_index) override {}
+    void setRoundOffset(uint8_t servo_index, int32_t ref_value) override {}
+    void setPositionGains(uint8_t servo_index) override {}
+    void setProfileVelocity(uint8_t servo_index) override {}
+    void setCurrentLimit(uint8_t servo_index) override {}
 
    private:
-    std::array<GxPwmServo, 4> servos_;
-    uint8_t servo_count_ = 0;
+    // Implement protected pure virtual functions from ServoBase
+    void setStatusReturnLevel() override {}
+    void getHomingOffset() override {}
+    void getCurrentLimit() override {}
+    void getPositionGains() override {}
+    void getProfileVelocity() override {}
+
+    TIM_HandleTypeDef* htim_ = nullptr;
+    std::array<float, MAX_SERVO_NUM> init_positions_;
     bool initialized_ = false;
 };
 
-#endif  // __GX_PWM_SERVO_DRIVER_H
+#endif
