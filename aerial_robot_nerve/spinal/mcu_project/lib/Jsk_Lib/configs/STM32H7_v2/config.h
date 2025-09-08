769c3e7496d9e6aa38d70c7916205e4da95aaf38
fjiwrogno <1948015867@qq.com>
2025-09-09 00:01:35 +0900

    Merge branch 'PR/servo/pwm-servo-GX3240MGV1' into dev/flamingo and configure pwm_htim2_ for sevo and bi-directonal esc.

--- aerial_robot_nerve/spinal/mcu_project/lib/Jsk_Lib/configs/STM32H7_v2/config.h
+++ aerial_robot_nerve/spinal/mcu_project/lib/Jsk_Lib/configs/STM32H7_v2/config.h
@@ -8,18 +8,19 @@
 #define __CONFIG_H
 #define SPINAL
 
-#if defined (STM32F756xx) || defined (STM32F746xx) || defined (STM32F745xx) || defined (STM32F765xx) || \
-    defined (STM32F767xx) || defined (STM32F769xx) || defined (STM32F777xx) || defined (STM32F779xx) || \
-    defined (STM32F722xx) || defined (STM32F723xx) || defined (STM32F732xx) || defined (STM32F733xx) || \
-    defined (STM32F730xx) || defined (STM32F750xx)
+#if defined(STM32F756xx) || defined(STM32F746xx) || defined(STM32F745xx) || defined(STM32F765xx) ||                    \
+    defined(STM32F767xx) || defined(STM32F769xx) || defined(STM32F777xx) || defined(STM32F779xx) ||                    \
+    defined(STM32F722xx) || defined(STM32F723xx) || defined(STM32F732xx) || defined(STM32F733xx) ||                    \
+    defined(STM32F730xx) || defined(STM32F750xx)
 #include "stm32f7xx_hal.h"
 #define STM32F7
 #endif
 
-#if defined (STM32H743xx) || defined (STM32H753xx)  || defined (STM32H750xx) || defined (STM32H742xx) || \
-    defined (STM32H745xx) || defined (STM32H755xx)  || defined (STM32H747xx) || defined (STM32H757xx) || \
-    defined (STM32H7A3xx) || defined (STM32H7A3xxQ) || defined (STM32H7B3xx) || defined (STM32H7B3xxQ) || defined (STM32H7B0xx)  || defined (STM32H7B0xxQ) || \
-    defined (STM32H735xx) || defined (STM32H733xx)  || defined (STM32H730xx) || defined (STM32H730xxQ)  || defined (STM32H725xx) || defined (STM32H723xx)
+#if defined(STM32H743xx) || defined(STM32H753xx) || defined(STM32H750xx) || defined(STM32H742xx) ||                    \
+    defined(STM32H745xx) || defined(STM32H755xx) || defined(STM32H747xx) || defined(STM32H757xx) ||                    \
+    defined(STM32H7A3xx) || defined(STM32H7A3xxQ) || defined(STM32H7B3xx) || defined(STM32H7B3xxQ) ||                  \
+    defined(STM32H7B0xx) || defined(STM32H7B0xxQ) || defined(STM32H735xx) || defined(STM32H733xx) ||                   \
+    defined(STM32H730xx) || defined(STM32H730xxQ) || defined(STM32H725xx) || defined(STM32H723xx)
 #include "stm32h7xx_hal.h"
 #define STM32H7
 #endif
@@ -28,43 +29,43 @@
 #define GPIO_H(port, pin) HAL_GPIO_WritePin(port, pin, GPIO_PIN_SET)
 #define GPIO_L(port, pin) HAL_GPIO_WritePin(port, pin, GPIO_PIN_RESET)
 
-//0. Comm Type  
+// 0. Comm Type
 #define NERVE_COMM 0
 
-//1. Specials board options
+// 1. Specials board options
 #define STM32H7_V2 1
 
-//2. Enable Flags
+// 2. Enable Flags
 //* Please set/reset follwing flags according to your utility.
 
-//2.1 Sensors
-//2.1.1 IMU Sensor
+// 2.1 Sensors
+// 2.1.1 IMU Sensor
 #define IMU_FLAG 1
 #define IMU_ICM 1
 #define IMU_MPU 0
-//2.1.2 Barometer Sensor
+// 2.1.2 Barometer Sensor
 #define BARO_FLAG 1
-//2.1.3 GPS Sensor
+// 2.1.3 GPS Sensor
 #define GPS_FLAG 0
-//2.1.3 Direct Servo Control
+// 2.1.3 Direct Servo Control
 #define SERVO_FLAG 1
-#define DYNAMIXEL 1
+#define DYNAMIXEL 0
 #define KONDO 0
-#define GX_PWM_SERVO 0
-//2.1.3.1 Dynamixel Servo Control without external convertor board
+#define GX_PWM_SERVO 1
+// 2.1.3.1 Dynamixel Servo Control without external convertor board
 #define DYNAMIXEL_BOARDLESS_CONTROL 0
-//2.2 State Estimate
-//2.2.1 Attitude Estimate
+// 2.2 State Estimate
+// 2.2.1 Attitude Estimate
 #define ATTITUDE_ESTIMATE_FLAG 1
 //* Do not change following code!!!
 ///////////////////////////////////
 #if !IMU_FLAG
 #undef ATTITUDE_ESTIMATE_FLAG
-#define  ATTITUDE_ESTIMATE_FLAG 0
+#define ATTITUDE_ESTIMATE_FLAG 0
 #endif
 ///////////////////////////////////
 
-//2.2.2 Height Estimate
+// 2.2.2 Height Estimate
 #define HEIGHT_ESTIMATE_FLAG 1
 //* Do not change following code!!!
 ///////////////////////////////////
@@ -74,7 +75,7 @@
 #endif
 ///////////////////////////////////
 
-//2.2.3 Pos Estimate
+// 2.2.3 Pos Estimate
 #define POS_ESTIMATE_FLAG 1
 //* Do not change following code!!!
 ///////////////////////////////////
@@ -84,7 +85,7 @@
 #endif
 ///////////////////////////////////
 
-//2.3 Flight Control
+// 2.3 Flight Control
 #define FLIGHT_CONTROL_FLAG 1
 #define DSHOT 0
 //* Do not change following code!!!
@@ -95,6 +96,4 @@
 #endif
 ///////////////////////////////////
 
-
-
-#endif //__CONFIG_H
+#endif  //__CONFIG_H
--- aerial_robot_nerve/spinal/mcu_project/lib/Jsk_Lib/servo/drivers/GX3240MGV1/gx_pwm_servo_driver.cpp
+++ aerial_robot_nerve/spinal/mcu_project/lib/Jsk_Lib/servo/drivers/GX3240MGV1/gx_pwm_servo_driver.cpp
@@ -1,92 +1,130 @@
 #include "gx_pwm_servo_driver.h"
 
-const std::array<uint32_t, MAX_SERVO_NUM> channels = {TIM_CHANNEL_1, TIM_CHANNEL_2, TIM_CHANNEL_3, TIM_CHANNEL_4};
+const std::array<uint32_t, MAX_SERVO_NUM> channels = { TIM_CHANNEL_1, TIM_CHANNEL_2, TIM_CHANNEL_3, TIM_CHANNEL_4 };
 
-bool GxPwmServo::init(TIM_HandleTypeDef* htim, uint8_t servo_num) {
-    if (servo_num > MAX_SERVO_NUM) return false;
-    // pwm config
-    htim_ = htim;
-    htim_->Instance = TIM4;
-    // Configure for 300Hz PWM, based on a 200MHz timer kernel clock.
-    // Timer clock = 200MHz. Prescaler = 19 => Tick Freq = 200M / 20 = 10MHz.
-    // Tick time = 0.1us. Period = 33332 => PWM Period = (33332+1) * 0.1us =
-    // 3333.3us => ~300Hz.
-    htim_->Init.Prescaler = 19;
-    htim_->Init.CounterMode = TIM_COUNTERMODE_UP;
-    htim_->Init.Period = 33332;
-    htim_->Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
-    htim_->Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
+bool GxPwmServo::init(TIM_HandleTypeDef* htim, uint8_t servo_num)
+{
+  if (servo_num > MAX_SERVO_NUM)
+    return false;
+  // pwm config
+  htim_ = htim;
+  htim_->Instance = TIM4;
 
-    HAL_TIM_Base_DeInit(htim_);
-    HAL_TIM_Base_Init(htim_);
-    HAL_TIM_PWM_Init(htim_);
+  HAL_TIM_PWM_Stop(htim_, TIM_CHANNEL_1);
+  HAL_TIM_Base_Stop(htim_);
+  HAL_TIM_Base_DeInit(htim_);
 
-    for (uint8_t i = 0; i < servo_num; ++i) {
-        FlashMemory::addValue(&init_positions_[i], sizeof(float));
-    }
-    FlashMemory::read();
+  htim_->Init.Prescaler = 199;
+  htim_->Init.CounterMode = TIM_COUNTERMODE_UP;  // Use standard UP mode
+  htim_->Init.Period = 3029;
 
-    for (uint8_t i = 0; i < servo_num; ++i) {
-        servo_[i] = ServoData(i + 1);
-        servo_[i].setGoalPositionRad(init_positions_[i]);
-        servo_[i].torque_enable_ = true;
-        HAL_TIM_PWM_Start(htim_, channels[i]);
-    }
-    servo_num_ = servo_num;
-    initialized_ = true;
+  while (HAL_TIM_Base_Init(htim_) != HAL_OK)
+    ;
+  while (HAL_TIM_PWM_Init(htim_) != HAL_OK)
+    ;
 
-    update();
+  TIM_OC_InitTypeDef sConfigOC = { 0 };
+  sConfigOC.OCMode = TIM_OCMODE_PWM1;
+  sConfigOC.OCPolarity = TIM_OCPOLARITY_HIGH;
+  sConfigOC.OCFastMode = TIM_OCFAST_DISABLE;
 
-    return true;
+  // --- Configure Channels 3 & 4 for bidirectional neutral throttle
+  // (1.5ms) ---
+  sConfigOC.Pulse = 1500;
+  while (HAL_TIM_PWM_ConfigChannel(htim_, &sConfigOC, TIM_CHANNEL_3) != HAL_OK)
+    ;
+  while (HAL_TIM_PWM_ConfigChannel(htim_, &sConfigOC, TIM_CHANNEL_4) != HAL_OK)
+    ;
+
+  HAL_TIM_Base_Start(htim_);
+
+  for (uint8_t i = 0; i < servo_num; ++i)
+  {
+    init_positions_[i] = M_PI;
+    FlashMemory::addValue(&init_positions_[i], sizeof(float));
+  }
+  FlashMemory::read();
+
+  for (uint8_t i = 0; i < servo_num; ++i)
+  {
+    servo_[i] = ServoData(i + 1);
+    servo_[i].setGoalPositionRad(init_positions_[i]);
+    servo_[i].torque_enable_ = true;
+  }
+
+  HAL_TIM_PWM_Start(htim_, TIM_CHANNEL_1);
+  HAL_TIM_PWM_Start(htim_, TIM_CHANNEL_2);
+  HAL_TIM_PWM_Start(htim_, TIM_CHANNEL_3);
+  HAL_TIM_PWM_Start(htim_, TIM_CHANNEL_4);
+
+  servo_num_ = servo_num;
+  initialized_ = true;
+
+  update();
+
+  return true;
 }
 
-void GxPwmServo::update() {
-    if (!initialized_ || !htim_) return;
-    for (uint8_t i = 0; i < servo_num_; ++i) {
-        float pulse_us = servo_[i].torque_enable_ ? angleToPulseWidth(servo_[i].getGoalPositionRad()) : 0.0f;
-        setCCR(channels[i], pulse_us);
-    }
+void GxPwmServo::update()
+{
+  if (!initialized_ || !htim_)
+    return;
+  for (uint8_t i = 0; i < servo_num_; ++i)
+  {
+    float pulse_us = servo_[i].torque_enable_ ? angleToPulseWidth(servo_[i].getGoalPositionRad()) : 0.0f;
+    setCCR(channels[i], pulse_us);
+  }
 }
 
-void GxPwmServo::setTorque(uint8_t servo_index) {
-    if (!initialized_ || servo_index >= servo_num_) return;
+void GxPwmServo::setTorque(uint8_t servo_index)
+{
+  if (!initialized_ || servo_index >= servo_num_)
+    return;
 
-    if (servo_[servo_index].torque_enable_ != true)
-    {
-        servo_[servo_index].torque_enable_ = !servo_[servo_index].torque_enable_;
-    }
+  if (servo_[servo_index].torque_enable_ != true)
+  {
+    servo_[servo_index].torque_enable_ = !servo_[servo_index].torque_enable_;
+  }
 }
 
-void GxPwmServo::setTorqueFromPresetnPos(uint8_t servo_index) {
-    if (!initialized_ || servo_index >= servo_num_) return;
-    if(servo_[servo_index].torque_enable_) servo_[servo_index].goal_position_ = servo_[servo_index].getPresentPosition();
-    servo_[servo_index].torque_enable_ = true;
+void GxPwmServo::setTorqueFromPresetnPos(uint8_t servo_index)
+{
+  if (!initialized_ || servo_index >= servo_num_)
+    return;
+  if (servo_[servo_index].torque_enable_)
+    servo_[servo_index].goal_position_ = servo_[servo_index].getPresentPosition();
+  servo_[servo_index].torque_enable_ = true;
 }
 
-void GxPwmServo::setAndSaveInitPosition(uint8_t id, float angle_rad) {
-    if (!initialized_ || id < 1 || id > servo_num_) return;
-    init_positions_[id - 1] = angle_rad;
-    FlashMemory::erase();
-    FlashMemory::write();
+void GxPwmServo::setAndSaveInitPosition(uint8_t id, float angle_rad)
+{
+  if (!initialized_ || id < 1 || id > servo_num_)
+    return;
+  init_positions_[id - 1] = angle_rad;
+  FlashMemory::erase();
+  FlashMemory::write();
 }
 
-float GxPwmServo::angleToPulseWidth(float angle_rad) const {
-    float pulse_width_us = MIN_PULSE_WIDTH_US + (angle_rad / ANGLE_RANGE_RAD) * (MAX_PULSE_WIDTH_US - MIN_PULSE_WIDTH_US);
-    
-    if (pulse_width_us < MIN_PULSE_WIDTH_US)
-    {
-        pulse_width_us = MIN_PULSE_WIDTH_US;
-    }
-    if (pulse_width_us > MAX_PULSE_WIDTH_US)
-    {
-        pulse_width_us = MAX_PULSE_WIDTH_US;
-    }
+float GxPwmServo::angleToPulseWidth(float angle_rad) const
+{
+  float pulse_width_us = MIN_PULSE_WIDTH_US + (angle_rad / ANGLE_RANGE_RAD) * (MAX_PULSE_WIDTH_US - MIN_PULSE_WIDTH_US);
 
-    return pulse_width_us;
+  if (pulse_width_us < MIN_PULSE_WIDTH_US)
+  {
+    pulse_width_us = MIN_PULSE_WIDTH_US;
+  }
+  if (pulse_width_us > MAX_PULSE_WIDTH_US)
+  {
+    pulse_width_us = MAX_PULSE_WIDTH_US;
+  }
+
+  return pulse_width_us;
 }
 
-void GxPwmServo::setCCR(uint32_t channel, float pulse_us) {
-    if (htim_ == nullptr) return;
-    uint32_t ccr_value = static_cast<uint32_t>(pulse_us / 0.1f);
-    __HAL_TIM_SET_COMPARE(htim_, channel, ccr_value);
+void GxPwmServo::setCCR(uint32_t channel, float pulse_us)
+{
+  if (htim_ == nullptr)
+    return;
+  uint32_t ccr_value = static_cast<uint32_t>(pulse_us / 0.1f);
+  __HAL_TIM_SET_COMPARE(htim_, channel, ccr_value);
 }
