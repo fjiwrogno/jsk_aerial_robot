c136acbc10bf1247945e2aa6f4a4e75af46eb5da
fjiwrogno <1948015867@qq.com>
2025-11-09 13:11:09 +0800

    [Flamingo][Underwater Mode] solve the bug about yaw control

--- robots/flamingo/src/control/flamingo_controller.cpp
+++ robots/flamingo/src/control/flamingo_controller.cpp
@@ -278,7 +278,10 @@ void FlamingoController::joyCallback(const sensor_msgs::Joy::ConstPtr& msg)
     total_thrust = 4.0;
   }
 
-  std::fill(target_base_thrust_.begin(), target_base_thrust_.end(), 0.0f);
+  // workaround: pad non-zero small value for base thrust (base_throttle) to avoid the
+  // unexpected condition check `fabs(base_thrust_term_[0]) > 0` in spinal.
+  // pelase refer to attitude_control.cpp, l.1175
+  std::fill(target_base_thrust_.begin(), target_base_thrust_.end(), 1e-6);
 
     // Assign thrust to the Z-component of each motor's virtual thrust vector
   for (int i = 0; i < motor_num_; ++i)
